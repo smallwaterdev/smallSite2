c091ccf03fb1f0f0e2b5bdb6bd174bb51cec1167
smallwaterdev <hatakusunoki@qwqwqwq.local>
2018-07-02 00:47:04 +0800

    update addr

--- src/app/services/config.js
+++ src/app/services/config.js
@@ -1,4 +1,5 @@
 
 //export const smallData_user_addr = "http://128.110.153.109:3000";
-export const smallData_user_addr = "http://192.168.1.100:3000";
+// export const smallData_user_addr = "http://192.168.1.100:3000";
 //export const smallData_user_addr = "http://54.193.2.203:3000";
+export const smallData_user_addr = "http://www.javferry.com:3000";
